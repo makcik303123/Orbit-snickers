0ee165e019b3892c4f192db64f10ff58cf68c17b
Maksim <adoberoflit@gmail.com>
2023-11-02 12:48:26 +0300

    add favicon

--- js/cookies.js
+++ js/cookies.js
@@ -39,9 +39,28 @@ function checkCookies() {
 
 function addMetrica() {
 	const script = document.createElement("script");
-	script.type = "module";
-	script.src = "js/observer.js";
+	script.type = "text/javascript";
+	script.innerHTML = `
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(95445044, "init", {
+      clickmap:true,
+      trackLinks:true,
+      accurateTrackBounce:true,
+      webvisor:true
+    });
+  `;
 	document.head.appendChild(script);
+
+	const noscript = document.createElement("noscript");
+	noscript.type = "text/javascript";
+	noscript.src = "https://mc.yandex.ru/watch/95445044";
+	noscript.async = true;
+	document.head.appendChild(noscript);
 }
 
 function confirmAnswer(index) {
